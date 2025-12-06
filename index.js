e5303860df26a660faedc6280142c39543514fb4
6ix7ven <andres.leonard02020987@gmail.com>
2025-12-06 10:29:12 -0300

    reconfigurando

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const { app } = require("./engine/engine");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.listen(3000, () => {
-	return console.log(`\x1b[1;32m[✓]\x1b[0m\tServer rodando: \x1b[1;4mhttp://0.0.0.0:${process.env.PORT || 3000}\x1b[0m`);
+app.use(express.static(path.join(__dirname,"dashboard")));
+app.use(express.urlencoded({ encoded: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname,"dashboard","index.html"));
+});
+
+app.get("/home", (req, res) => {
+	res.sendFile(path.join(__dirname,"dashboard","home.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	return console.log(`\x1b[1;32m[✓]\x1b[0m\tServer iniciado \x1b[1;4mhttp://127.0.0.1:${process.env.PORT || 3000}\x1b[0m`);
 });
